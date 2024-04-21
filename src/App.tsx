355fdb6c0a7f27445a2c60746921d225ed2fe4d2
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-04-21 23:15:41 +0630

    creat home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,16 @@
 import './App.css'
+import Carousel from './layouts/HomePage/Carousel'
+import ExploreTopBooks from './layouts/HomePage/ExploreTopBooks'
+import NavBar from './layouts/NavBarAndFooter/NavBar'
 
 function App() {
 
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-      <div className='container-fluid'>
-        <span className='navbar-brand'>Luv 2 Read</span>
-        <button className='navbar-toggler' type='button'
-          data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
-          aria-controls='navbarNavDropdown' aria-expanded='false'
-          aria-label='Toggle Navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <a className='nav-link'>Home</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' >Search Books</a>
-            </li>
-
-              <li className='nav-item'>
-                <a className='nav-link' >Shelf</a>
-              </li>
-            
-           
-              <li className='nav-item'>
-                <a className='nav-link' >Admin</a>
-              </li>
-            
-          </ul>
-          <ul className='navbar-nav ms-auto'>
-          
-              <li className='nav-item m-1'>
-                <a type='button' className='btn btn-outline-light'>Sign in</a>
-              </li>
-              <li>
-                <button className='btn btn-outline-light'>Logout</button>
-              </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <>
+      <NavBar/>
+      <ExploreTopBooks/>
+      <Carousel/>
+    </>
   )
 }
 
