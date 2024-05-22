f272d01401919baa273df6f724228a3393c1c2d7
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-05-22 23:06:19 +0630

    add checkout

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -5,14 +5,24 @@ import { StarsReview } from "../Utils/StarReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 import ReviewModel from "../../models/ReviewModel";
 import { LatestReviews } from "./LatestReviews";
+import { useOktaAuth } from "@okta/okta-react";
 
 export default function BookCheckoutPage() {
+  const { authState } = useOktaAuth();
+
   const [book, setBook] = useState<BookModel>();
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState(null);
-  const [reviews,setReviews] = useState<ReviewModel[]>([]);
-  const [totalStars,setTotalStars] = useState(0);
-  const [isLoadingReview,setIsLoadingReview] = useState(true);
+  const [reviews, setReviews] = useState<ReviewModel[]>([]);
+  const [totalStars, setTotalStars] = useState(0);
+  const [isLoadingReview, setIsLoadingReview] = useState(true);
+  const [isReviewLeft, setIsReviewLeft] = useState(false);
+  const [isLoadingUserReview, setIsLoadingUserReview] = useState(true);
+  const [isCheckedOut, setIsCheckedOut] = useState(false);
+  const [currentLoansCount, setCurrentLoansCount] = useState(0);
+  const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] =
+    useState(true);
+  const [isLoadingBookCheckedOut, setIsLoadingBookCheckedOut] = useState(true);
 
   const bookId = window.location.pathname.split("/")[2];
 
@@ -38,7 +48,7 @@ export default function BookCheckoutPage() {
         category: responseJson.category,
         img: responseJson.img,
       };
-      
+
       setBook(loadedBook);
       setIsLoading(false);
     };
@@ -49,49 +59,131 @@ export default function BookCheckoutPage() {
     });
   }, []);
 
-  useEffect(()=>{
-    const fetchBookReviews = async()=>{
+  useEffect(() => {
+    const fetchBookReviews = async () => {
       const reviewUrl: string = `http://localhost:8080/api/review/search/findByBookId?bookId=${bookId}`;
       const responseReviews = await fetch(reviewUrl);
 
-      if(!responseReviews.ok){
-        throw new Error('Something went wrong');
+      if (!responseReviews.ok) {
+        throw new Error("Something went wrong");
       }
 
       const responseJsonReview = await responseReviews.json();
       const responseData = responseJsonReview._embedded.reviews;
 
-      const loadedReviews : ReviewModel[] = [];
+      const loadedReviews: ReviewModel[] = [];
 
       let weightedStarReviews: number = 0;
-      
+
       for (const key in responseData) {
         loadedReviews.push({
-            id: responseData[key].id,
-            userEmail: responseData[key].userEmail,
-            date: responseData[key].date,
-            rating: responseData[key].rating,
-            book_id: responseData[key].bookId,
-            reviewDescription: responseData[key].reviewDescription,
+          id: responseData[key].id,
+          userEmail: responseData[key].userEmail,
+          date: responseData[key].date,
+          rating: responseData[key].rating,
+          book_id: responseData[key].bookId,
+          reviewDescription: responseData[key].reviewDescription,
         });
         weightedStarReviews = weightedStarReviews + responseData[key].rating;
-    }
-      
-    if (loadedReviews) {
-      const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
-      setTotalStars(Number(round));
-  }
-  setReviews(loadedReviews);
-  setIsLoadingReview(false);
+      }
+
+      if (loadedReviews) {
+        const round = (
+          Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2
+        ).toFixed(1);
+        setTotalStars(Number(round));
+      }
+      setReviews(loadedReviews);
+      setIsLoadingReview(false);
     };
     fetchBookReviews().catch((error: any) => {
       setIsLoadingReview(false);
       setHttpError(error.message);
-  })
+    });
+  }, []);
 
-  },[])
+  useEffect(() => {
+    const fetchUserReviewBook = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/reviews/secure/user/book/?bookId=${bookId}`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const userReview = await fetch(url, requestOptions);
+        if (!userReview.ok) {
+          throw new Error("Something went wrong");
+        }
+        const userReviewResponseJson = await userReview.json();
+        setIsReviewLeft(userReviewResponseJson);
+      }
+      setIsLoadingUserReview(false);
+    };
+    fetchUserReviewBook().catch((error: any) => {
+      setIsLoadingUserReview(false);
+      setHttpError(error.message);
+    });
+  }, [authState]);
 
-  if (isLoading || isLoadingReview) {
+  useEffect(() => {
+    const fetchUserCurrentLoansCount = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/books/secure/currentloans/count`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const currentLoansCountResponse = await fetch(url, requestOptions);
+        if (!currentLoansCountResponse.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const currentLoansCountResponseJson =
+          await currentLoansCountResponse.json();
+        setCurrentLoansCount(currentLoansCountResponseJson);
+      }
+      setIsLoadingCurrentLoansCount(false);
+    };
+    fetchUserCurrentLoansCount().catch((error: any) => {
+      setIsLoadingCurrentLoansCount(false);
+      setHttpError(error.message);
+    });
+  }, [authState, isCheckedOut]);
+
+  useEffect(() => {
+    const fetchUserCheckedOutBook = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/books/secure/ischeckedout/byuser/?bookId=${bookId}`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const bookCheckedOut = await fetch(url, requestOptions);
+
+        if (!bookCheckedOut.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const bookCheckedOutResponseJson = await bookCheckedOut.json();
+        setIsCheckedOut(bookCheckedOutResponseJson);
+      }
+      setIsLoadingBookCheckedOut(false);
+    };
+    fetchUserCheckedOutBook().catch((error: any) => {
+      setIsLoadingBookCheckedOut(false);
+      setHttpError(error.message);
+    });
+  }, [authState]);
+
+  if (isLoading || isLoadingReview || isLoadingCurrentLoansCount) {
     return <SpinnerLoading />;
   }
 
@@ -103,56 +195,94 @@ export default function BookCheckoutPage() {
     );
   }
 
+  async function checkoutBook() {
+    const url = `http://localhost:8080/api/books/secure/checkout/?bookId=${book?.id}`;
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+        "Content-Type": "application/json",
+      },
+    };
+    const checkoutResponse = await fetch(url, requestOptions);
+    if (!checkoutResponse.ok) {
+      throw new Error("Something went wrong!");
+    }
+    setIsCheckedOut(true);
+  }
+
   return (
     <div>
-            <div className='container d-none d-lg-block'>
-                <div className='row mt-5'>
-                    <div className='col-sm-2 col-md-2'>
-                        {book?.img ?
-                            <img src={book?.img} width='226' height='349' alt='Book' />
-                            :
-                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
-                                height='349' alt='Book' />
-                        }
-                    </div>
-                    <div className='col-4 col-md-4 container'>
-                        <div className='ml-2'>
-                            <h2>{book?.title}</h2>
-                            <h5 className='text-primary'>{book?.author}</h5>
-                            <p className='lead'>{book?.description}</p>
-                            <StarsReview rating={totalStars} size={32} />
-                        </div>
-                    </div>
-                    <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount} 
-                        isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} 
-                        checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} submitReview={submitReview}/>
-                </div>
-                <hr />
-                <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
+      <div className="container d-none d-lg-block">
+        <div className="row mt-5">
+          <div className="col-sm-2 col-md-2">
+            {book?.img ? (
+              <img src={book?.img} width="226" height="349" alt="Book" />
+            ) : (
+              <img
+                src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                width="226"
+                height="349"
+                alt="Book"
+              />
+            )}
+          </div>
+          <div className="col-4 col-md-4 container">
+            <div className="ml-2">
+              <h2>{book?.title}</h2>
+              <h5 className="text-primary">{book?.author}</h5>
+              <p className="lead">{book?.description}</p>
+              <StarsReview rating={totalStars} size={32} />
             </div>
-            <div className='container d-lg-none mt-5'>
-                <div className='d-flex justify-content-center alighn-items-center'>
-                    {book?.img ?
-                        <img src={book?.img} width='226' height='349' alt='Book' />
-                        :
-                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
-                            height='349' alt='Book' />
-                    }
-                </div>
-                <div className='mt-4'>
-                    <div className='ml-2'>
-                        <h2>{book?.title}</h2>
-                        <h5 className='text-primary'>{book?.author}</h5>
-                        <p className='lead'>{book?.description}</p>
-                        <StarsReview rating={totalStars} size={32} />
-                    </div>
-                </div>
-                <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount} 
-                    isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} 
-                    checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} submitReview={submitReview}/>
-                <hr />
-                <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
-            </div>
+          </div>
+          <CheckoutAndReviewBox
+            book={book}
+            mobile={false}
+            currentLoansCount={currentLoansCount}
+            isAuthenticated={authState?.isAuthenticated}
+            isCheckedOut={isCheckedOut}
+            checkoutBook={checkoutBook}
+            isReviewLeft={isReviewLeft}
+            submitReview={submitReview}
+          />
         </div>
+        <hr />
+        <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
+      </div>
+      <div className="container d-lg-none mt-5">
+        <div className="d-flex justify-content-center alighn-items-center">
+          {book?.img ? (
+            <img src={book?.img} width="226" height="349" alt="Book" />
+          ) : (
+            <img
+              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+              width="226"
+              height="349"
+              alt="Book"
+            />
+          )}
+        </div>
+        <div className="mt-4">
+          <div className="ml-2">
+            <h2>{book?.title}</h2>
+            <h5 className="text-primary">{book?.author}</h5>
+            <p className="lead">{book?.description}</p>
+            <StarsReview rating={totalStars} size={32} />
+          </div>
+        </div>
+        <CheckoutAndReviewBox
+          book={book}
+          mobile={true}
+          currentLoansCount={currentLoansCount}
+          isAuthenticated={authState?.isAuthenticated}
+          isCheckedOut={isCheckedOut}
+          checkoutBook={checkoutBook}
+          isReviewLeft={isReviewLeft}
+          submitReview={submitReview}
+        />
+        <hr />
+        <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
+      </div>
+    </div>
   );
 }
