d2e8f8c169c69dc96485e5cb18986b8e0b0b9cc6
KaungMyat-K <kaungmyat.dev8@gmail.com>
2024-07-28 20:49:39 +0630

    add admin message and admin messages page

--- src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -1,6 +1,7 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
+import AdminMessages from "./components/AdminMessages";
 
 export default function ManageLibraryPage() {
   
